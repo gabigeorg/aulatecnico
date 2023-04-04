23603de4c0a0b1bc8eb58553247258d3a25ae6e7
Gabriela <gabrielageorg4@gmail.com>
2023-04-04 12:26:29 -0300

    teste

---
+++ main.js
@@ -0,0 +1,100 @@
+//comentário
+/* comentário mais longo*/
+
+// console.log("Olá mundo!")
+
+// const nome = "Gabriela"
+// let idade = 16
+
+// console.log(nome)
+// console.log(idade)
+
+// const idadeAluno = 28
+
+// const nomeAluna = "Eduarda"
+
+// const triste = false
+
+// console.log(idadeAluno)
+// console.log(nomeAluna)
+// console.log(triste)
+
+// const pergunta = prompt("Qual o seu nome?")
+// console.log(pergunta)
+
+// const nome = "Bibiana"
+// let idade = 27
+// const sobrenome = "Feliz"
+// const sono = true
+
+// console.log(nome)
+// console.log(idade)
+// console.log(sobrenome)
+// console.log(sono)
+
+// const nome = "Gabi"
+// const idade = 16
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+// console.log("Meu nome é", nome, "e tenho", idade, "anos")
+// console.log("Meu nome é "+ nome+ " e tenho "+ idade+ " anos")
+// console.log(`Meu nome é ${nome} e tenho ${idade} anos`)
+
+
+// const pergunta = Number(prompt("Digite um número"))
+// console.log(typeof pergunta)
+// console.log(pergunta)
+
+// const pergunta2 = Number(prompt("Digite outro número"))
+// console.log(pergunta2)
+
+// const soma = pergunta + pergunta2
+// console.log(soma)
+
+// const nome = "Gabriela"
+// const idade = 16
+
+// console.log(`Olá ${nome}, você tem ${idade} anos`)
+
+// const variav = 12
+// console.log(typeof variav)
+
+// console.log(typeof variav.toString())
+
+// const nome = "Eduarda é fofinha é engraçadinha também"
+
+// console.log(nome.length)
+// console.log(nome.toUpperCase())
+// console.log(nome.toLowerCase())
+// console.log(nome.trim())
+// console.log(nome.includes("Eduarda"))
+// console.log(nome.replace("é", "muito"))
+
+
+
+// const pergunta = (prompt("Escreva uma frase"))
+// pergunta = (pergunta.toUpperCase())
+// console.log(pergunta)
+
+const arrayLista = ["item1", "item2", "item3"]
+
+const mercadoLista = ["pessego","manga", "uva", "leite", "ovo", "frango"]
+
+const escolaLista = ["lápis", "tesoura", "caneta", "caderno", "livro", "régua"]
+
+
+console.log(arrayLista[0])
+
+const posicao = Number(prompt("Insira um número de 1 a 6:"))
+console.log(mercadoLista[posicao -1])
+
+console.log(mercadoLista.length)
+console.log(mercadoLista.push("Melão"))
+console.log(mercadoLista)
+console.log(mercadoLista.pop())
+console.log(mercadoLista.length)
+console.log(mercadoLista)
+console.log(splice(0,3))
+console.log(mercadoLista)
